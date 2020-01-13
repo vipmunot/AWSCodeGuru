ee153fe9b8daab92706546b45e435108e2876bee
Munot <munvipul@38f9d34e8f7c.ant.amazon.com>
2020-01-13 12:45:01 -0500

    changed Testing

--- HelloWorld/src/main/java/org/example/App.java
+++ HelloWorld/src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello AWSCodeGuru!. Lets start testing" 
+        System.out.println( "Hello AWSCodeGuru!. Lets start testing" );
     }
 }
